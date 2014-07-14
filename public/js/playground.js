3c3f84ebed936dac298b9d1d944203742a714301
Paul Dombkowski <pdombkowski@gmail.com>
2014-07-14 15:45:27 -0500

    added call to server closure and tested within success method for printing to DOM

--- public/js/playground.js
+++ public/js/playground.js
@@ -1,13 +1,33 @@
 var endpointCount = 77;
+var returnArray = [];
+var ajaxCalls = {};
 for (var i = 0; i < endpointCount; i++) {
-	// Make a call to our server, requesting a set of data.
-	// The data we get back is a chunk of strings that form
-	// a larger message.
-	$.ajax({
-		type: 'GET',
-		url: '/' + i,
-		success: function() {
-			console.log('Got back success from call ' + i + '!');
-		}
-	});
+  (function (i) {
+    returnArray.push(undefined);
+    ajaxCalls[i] = false;
+    $.ajax({
+      type: 'GET',
+      url: '/' + i,
+      dataType: 'json',
+      success: function(valueObject) {
+        ajaxCalls[i] = true;
+        valString = "";
+        for (val in valueObject) {
+          valString += valueObject[val];
+        }
+        returnArray[i] = valString;
+
+        // Test if complete
+        var test_complete = true;
+        for (i in ajaxCalls) {
+          test_complete = test_complete && ajaxCalls[i];
+        }
+        if (test_complete) {
+          $('#container').css("white-space", "pre");
+          $('#container').text(returnArray.join('')).trim();
+        }
+      }
+    });
+  })(i);
 }
+
